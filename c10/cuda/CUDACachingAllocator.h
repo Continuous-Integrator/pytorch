0919d01f216cbda88a9488b30075c674df16ff49
Frank Lin <eee4017@gmail.com>
2026-02-20 01:00:46 +0000

    Fix user MemPool incorrectly inherits CUDA graph capture behavior in CUDACachingAllocator

--- c10/cuda/CUDACachingAllocator.h
+++ c10/cuda/CUDACachingAllocator.h
@@ -146,7 +146,8 @@ class CUDAAllocator : public DeviceAllocator {
   virtual void beginAllocateToPool(
       c10::DeviceIndex device,
       MempoolId_t mempool_id,
-      std::function<bool(cudaStream_t)> filter) = 0;
+      std::function<bool(cudaStream_t)> filter,
+      bool is_graph_capture = false) = 0;
   virtual void endAllocateToPool(
       c10::DeviceIndex device,
       MempoolId_t mempool_id) = 0;
@@ -377,8 +378,10 @@ inline CheckpointDelta setCheckpointPoolState(
 inline void beginAllocateToPool(
     c10::DeviceIndex device,
     MempoolId_t mempool_id,
-    std::function<bool(cudaStream_t)> filter) {
-  get()->beginAllocateToPool(device, mempool_id, std::move(filter));
+    std::function<bool(cudaStream_t)> filter,
+    bool is_graph_capture = false) {
+  get()->beginAllocateToPool(
+      device, mempool_id, std::move(filter), is_graph_capture);
 }
 
 inline void endAllocateToPool(c10::DeviceIndex device, MempoolId_t mempool_id) {
