d654e7dafef6a6d7b927d1d67496ce6bcfb2b620
Ke Wen <kwen@nvidia.com>
2026-03-25 00:03:02 -0700

    Update
    [ghstack-poisoned]

--- torch/csrc/distributed/c10d/symm_mem/ops/nccl_all_to_all_permute.cu
+++ torch/csrc/distributed/c10d/symm_mem/ops/nccl_all_to_all_permute.cu
@@ -1,9 +1,3 @@
-// Copyright (c) Meta Platforms, Inc. and affiliates.
-// All rights reserved.
-//
-// This source code is licensed under the BSD-style license found in the
-// LICENSE file in the root directory of this source tree.
-
 #include <c10/cuda/CUDAGuard.h>
 #include <c10/macros/Macros.h>
 #include <ATen/native/cuda/MemoryAccess.cuh>
