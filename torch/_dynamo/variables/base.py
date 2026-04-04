02426d6a5e9d8713cdfa911e3b166dc897701e54
Guilherme Leobas <gleobas@quansight.com>
2026-04-04 01:42:04 +0000

    Update (base update)
    [ghstack-poisoned]

--- torch/_dynamo/variables/base.py
+++ torch/_dynamo/variables/base.py
@@ -594,6 +594,10 @@ class VariableTracker(metaclass=VariableTrackerMeta):
             from .object_protocol import generic_getiter
 
             return generic_getiter(tx, self)
+        elif name == "__next__" and not args and not kwargs:
+            from .object_protocol import generic_iternext
+
+            return generic_iternext(tx, self)
         elif (
             name == "__getattr__"
             and len(args) == 1
