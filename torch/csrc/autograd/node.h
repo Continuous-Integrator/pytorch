42e8b6e91a4f6a55ff52ce31af87a25309dd0087
Sam Gross <colesbury@gmail.com>
2026-04-08 23:26:41 +0000

    Update (base update)
    [ghstack-poisoned]

--- torch/csrc/autograd/node.h
+++ torch/csrc/autograd/node.h
@@ -27,7 +27,7 @@ struct Edge;
 struct FunctionPostHook;
 struct FunctionPreHook;
 struct Node;
-struct SavedVariable;
+class SavedVariable;
 
 using Variable = at::Tensor;
 using tensor_list = std::vector<at::Tensor>;
