68f1b15b90a18709b030b00b441fb1a0edbb428f
NikhilAPatel <nikhilap@meta.com>
2026-04-18 05:49:49 -0700

    Update on "Fix Incorrect storage offsets propagation in inductor with as_strided"
    Fix for https://github.com/pytorch/pytorch/issues/175325
    
    cc voznesenskym penguinwu EikanWang jgong5 Guobing-Chen XiaobingSuper zhuhaozhe blzheng wenzhe-nrv jiayisunx ipiszy kadeng muchulee8 amjames chauhang aakhundov coconutruben jataylo
    
    [ghstack-poisoned]

--- test/inductor/test_torchinductor_codegen_dynamic_shapes.py
+++ test/inductor/test_torchinductor_codegen_dynamic_shapes.py
@@ -113,6 +113,9 @@ test_failures = {
     "test_as_strided_on_views_dynamic_shapes": TestFailure(
         ("cpu", "cuda", "xpu"), is_skip=True
     ),
+    "test_as_strided_on_split_view_dynamic_shapes": TestFailure(
+        ("cpu", "cuda", "xpu"), is_skip=True
+    ),
     #
     # Failed to find dynamic for loop variable:
     #
