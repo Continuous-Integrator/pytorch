3140c544ebbf8d81018851b75b50110f19c26fad
Animesh Jain <anijain@umich.edu>
2026-04-18 21:07:19 -0700

    [dynamo] Support pybind enums
    ghstack-source-id: a8f02aa282b220b144438cd6477b790588c4aced
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/180631

--- torch/_dynamo/variables/builder.py
+++ torch/_dynamo/variables/builder.py
@@ -160,6 +160,7 @@ from ..utils import (
     is_lru_cache_wrapped_function,
     is_namedtuple,
     is_parameter_freezing,
+    is_pybind11_enum_member,
     is_typing,
     is_utils_checkpoint,
     is_wrapper_or_member_descriptor,
@@ -979,7 +980,7 @@ class VariableBuilder:
         elif isinstance(
             value,
             (enum.Enum, torch.DispatchKey, torch._C._functorch.TransformType),
-        ):
+        ) or is_pybind11_enum_member(value):
             self.install_guards(GuardBuilder.ID_MATCH)
             return UserDefinedObjectVariable(value, source=self.source)
         elif DebuggingVariable.is_reorderable_logging_function(value):
@@ -4267,7 +4268,11 @@ class SourcelessBuilder:
         if isinstance(value, VariableTracker):
             # This is always valid to call, and useful for recursive calls.
             return value
-        elif is_opaque_value_type(type(value)) and not isinstance(value, enum.Enum):
+        elif (
+            is_opaque_value_type(type(value))
+            and not isinstance(value, enum.Enum)
+            and not is_pybind11_enum_member(value)
+        ):
             return TorchScriptObjectVariable.create(value, value)
         elif is_opaque_reference_type(type(value)):
             # This is for handling opaque objects in custom ops
@@ -4301,7 +4306,7 @@ class SourcelessBuilder:
         elif isinstance(
             value,
             (enum.Enum, torch.DispatchKey, torch._C._functorch.TransformType),
-        ):
+        ) or is_pybind11_enum_member(value):
             return UserDefinedObjectVariable(value)
         elif isinstance(value, (type, abc.ABCMeta)):
             if issubclass(type(value), type) and issubclass(value, BaseException):
