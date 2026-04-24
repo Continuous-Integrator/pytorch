dbd8af48da1def0f77077c6d7e711c501fab8a4f
Pian Pawakapan <pianpwk@meta.com>
2026-04-24 11:07:00 -0700

    Update (base update)
    [ghstack-poisoned]

--- torch/spmd_types.py
+++
@@ -1,3 +0,0 @@
-# Backward-compatibility shim: use torch.distributed.spmd_types instead.
-from torch.distributed.spmd_types import *  # noqa: F403
-from torch.distributed.spmd_types import _reset, is_available  # noqa: F401
