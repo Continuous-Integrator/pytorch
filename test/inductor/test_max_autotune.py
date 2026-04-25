2dce4aa309f34cad384dc7695e8236d84462adfd
bobrenjc93 <bobren@meta.com>
2026-04-25 13:39:19 -0700

    Update (base update)
    [ghstack-poisoned]

--- test/inductor/test_max_autotune.py
+++ test/inductor/test_max_autotune.py
@@ -4234,11 +4234,6 @@ class TestPrologueFusion(TestCase):
             )
         )
 
-    @classmethod
-    def tearDownClass(cls):
-        cls._stack.close()
-        super().tearDownClass()
-
     def check_code(self, code_str, num_kernels, num_allocs, num_deallocs):
         FileCheck().check(get_func_call()).check_count(
             get_kernel_launch(),
@@ -4745,11 +4740,6 @@ class TestEpilogueFusionStaticAnalysis(TestCase):
                 }
             )
         )
-
-    @classmethod
-    def tearDownClass(cls):
-        cls._stack.close()
-        super().tearDownClass()
 
     @contextlib.contextmanager
     def get_common_patches(
