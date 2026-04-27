9e92bfd959d349390bce0d4ad42f8d28ecbd59dd
Liqiang Lu <liqiangxl@gmail.com>
2026-04-27 12:49:36 -0700

    lint

--- test/inductor/test_torchinductor.py
+++ test/inductor/test_torchinductor.py
@@ -27,7 +27,6 @@ from typing_extensions import ParamSpec
 from unittest.mock import patch
 
 import numpy as np
-from torch.testing._internal.common_cuda import SM100OrLater
 import torch
 import torch._dynamo.config as dynamo_config
 import torch._inductor.aoti_eager
@@ -74,6 +73,7 @@ from torch.testing._internal.common_cuda import (
     IS_SM90,
     PLATFORM_SUPPORTS_FLASH_ATTENTION,
     PLATFORM_SUPPORTS_MEM_EFF_ATTENTION,
+    SM100OrLater,
     SM80OrLater,
     SM90OrLater,
     TEST_CUDNN,
@@ -16675,13 +16675,11 @@ if RUN_GPU:
                     # one kernel, with extra workspace/semaphore args
                     0: (0, 1, 2, 3, 5),
                 }
-            elif(
-                SM100OrLater
-            ):
+            elif SM100OrLater:
                 self.assertEqual(len(kernels), 1)
                 expected_divisible = {
-                     0: (0, 1, 3),
-                 }
+                    0: (0, 1, 3),
+                }
             else:
                 self.assertEqual(len(kernels), 2)
 
