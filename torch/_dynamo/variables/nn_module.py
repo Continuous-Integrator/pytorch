fb0d15101ab4dbcd54c99f31956becba7a09cfc0
William Wen <williamwen@meta.com>
2026-04-29 11:41:43 -0700

    [dynamo, nested graph breaks] disable nested graph breaks on inline_user_function_return by default
    ghstack-source-id: c3e346493ac59dd487eb69e6da2a0dd7aa4688a6
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/179844

--- torch/_dynamo/variables/nn_module.py
+++ torch/_dynamo/variables/nn_module.py
@@ -586,6 +586,7 @@ class NNModuleVariable(VariableTracker):
                     variables.UserFunctionVariable(fn, source=fn_source),
                     args,
                     kwargs,
+                    allow_nested_graph_breaks=True,
                 )
 
     def mp_subscript_impl(
@@ -631,6 +632,7 @@ class NNModuleVariable(VariableTracker):
                 variables.UserFunctionVariable(fn, source=src),
                 [self, key],
                 {},
+                allow_nested_graph_breaks=True,
             )
 
         if isinstance(key, SliceVariable):
