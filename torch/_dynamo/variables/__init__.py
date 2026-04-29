99799c6f0182436cff8a8542bceecce30e092e22
Yu, Guangye <guangye.yu@intel.com>
2026-04-29 14:31:16 +0000

    Update (base update)
    [ghstack-poisoned]

--- torch/_dynamo/variables/__init__.py
+++ torch/_dynamo/variables/__init__.py
@@ -27,6 +27,7 @@ from .builtin import (
 )
 from .constant import ConstantVariable
 from .ctx_manager import (
+    AcceleratorDeviceIndexVariable,
     CatchWarningsCtxManagerVariable,
     ContextWrappingVariable,
     CUDADeviceVariable,
@@ -186,6 +187,7 @@ from .user_defined import (
 
 
 __all__ = [
+    "AcceleratorDeviceIndexVariable",
     "AutogradFunctionContextVariable",
     "AutogradFunctionVariable",
     "BackwardHookVariable",
