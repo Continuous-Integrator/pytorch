39a4333e4ce1109be4e3053dd5d0e6133b1b1222
Yu, Guangye <guangye.yu@intel.com>
2026-04-29 14:31:16 +0000

    Update
    [ghstack-poisoned]

--- torch/_dynamo/variables/__init__.py
+++ torch/_dynamo/variables/__init__.py
@@ -50,6 +50,7 @@ from .ctx_manager import (
     VmapIncrementNestingCtxManagerVariable,
     WithEnterFunctionVariable,
     WithExitFunctionVariable,
+    XPUDeviceVariable,
 )
 from .dicts import (
     ConstDictVariable,
@@ -187,6 +188,7 @@ from .user_defined import (
 
 
 __all__ = [
+    "XPUDeviceVariable",
     "AcceleratorDeviceIndexVariable",
     "AutogradFunctionContextVariable",
     "AutogradFunctionVariable",
