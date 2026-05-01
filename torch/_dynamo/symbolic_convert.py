80f360b930e1c94a8ab6939b104cf2b610c70620
zhxchen17 <zhxchen17@fb.com>
2026-05-01 07:29:08 -0700

    [dynamo] Python codegen for simple fullgraph case POC. [1/n]
    Summary:
    
    This PR adds a feature to optionally generate python wrapper code in addition to the bytecode wrapper, such that it has the same behavior for simple
    fullgraph cases. This is useful when we are compiling a module/function and want all part to be savable as Python for debugability or portability
    reasons.
    
    This option is introduced by opting in with `torch._dynamo.generate_pycode=True` and will be enabled when fullgraph_capture() is used. NOTE that
    to minimize the landing risk, user also need to set use_python_codegen=True to activate this at runtime.
    
    Sample generated python wrapper code:
    
    ```
    __arg0 = self._modules['linear']._parameters['weight']
    __arg1 = self._modules['linear']._parameters['bias']
    __arg2 = x
    __graph_out = __compiled_fn_<ID>(__arg0, __arg1, __arg2)
    __stack0 = __graph_out[0]
    __ret = __stack0""",
    ```
    
    Test Plan:
    
    pytest test/dynamo/test_pycode.py
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:
    
    ghstack-source-id: 365018e0521baa7f56e2e02c1775cca945bcbada
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/182090

--- torch/_dynamo/symbolic_convert.py
+++ torch/_dynamo/symbolic_convert.py
@@ -5173,7 +5173,8 @@ class InstructionTranslator(InstructionTranslatorBase):
         assert len(all_stack_locals_metadata) == 1
         assert not all_stack_locals_metadata[0].stack_null_idxes
         self.output.add_output_instructions(
-            self.codegen_return_with_pops(inst, all_stack_locals_metadata[0].num_stack)
+            self.codegen_return_with_pops(inst, all_stack_locals_metadata[0].num_stack),
+            "__ret = __stack0",
         )
         raise ReturnValueOp
 
